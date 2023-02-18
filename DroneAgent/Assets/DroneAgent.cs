c1703101453071f5a29881fa2c1e153244614ec8
nucontreras <48302692+nucontreras@users.noreply.github.com>
2023-02-18 19:30:40 +0100

    up & down movements

--- DroneAgent/Assets/DroneAgent.cs
+++ DroneAgent/Assets/DroneAgent.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class DroneAgent : MonoBehaviour
 {
-    
+    Rigidbody ourDrone;
+    public float upForce;
+    void Awake()
+    {
+        ourDrone = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        MovementUpDown();
+        ourDrone.AddRelativeForce(Vector3.up * upForce);
+    }
+    void MovementUpDown()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            upForce = 450f;
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            upForce = -200f;
+        } 
+        else if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K))
+        {
+            upForce = 98.1f;
+        }
+    }
 }
