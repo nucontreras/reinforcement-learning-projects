8d88a4bcdce849cc49e6fc67a31491ff221f8262
nucontreras <48302692+nucontreras@users.noreply.github.com>
2023-02-18 21:42:50 +0100

    rotations

--- DroneAgent/Assets/DroneAgent.cs
+++ DroneAgent/Assets/DroneAgent.cs
@@ -6,19 +6,26 @@ public class DroneAgent : MonoBehaviour
 {
     Rigidbody ourDrone;
     public float upForce;
-    private float movementForwardSpeed = 0.5f;  // 500
+    private float movementForwardSpeed = 100f;  // 500
     private float tiltAmountForward = 0f;
     private float tiltVelocityForward;
+    private float wantedYRotation;
+    private float currentYRotation;
+    private float rotateAmoutByKeys = 2.5f;
+    private float rotationYVelocity;
+
     void Awake()
     {
         ourDrone = GetComponent<Rigidbody>();
+        //ourDrone.transform.localRotation = Quaternion.Euler(0, 180, 0);
     }
     private void FixedUpdate()
     {
         MovementUpDown();
         MovementForward();
+        Rotation();
         ourDrone.AddRelativeForce(Vector3.up * upForce);
-        ourDrone.rotation = Quaternion.Euler(new Vector3(tiltAmountForward, ourDrone.rotation.y, ourDrone.rotation.z));
+        ourDrone.rotation = Quaternion.Euler(new Vector3(tiltAmountForward, currentYRotation, ourDrone.rotation.z));
     }
     void MovementUpDown()
     {
@@ -42,5 +49,17 @@ public class DroneAgent : MonoBehaviour
             ourDrone.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * movementForwardSpeed);
             tiltAmountForward = Mathf.SmoothDamp(tiltAmountForward, 20 * Input.GetAxis("Vertical"), ref tiltVelocityForward, 0.1f);
         }
+    }
+    void Rotation()
+    {
+        if (Input.GetKey(KeyCode.J))
+        {
+            wantedYRotation -= rotateAmoutByKeys;
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            wantedYRotation += rotateAmoutByKeys;
+        }
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, wantedYRotation, ref rotationYVelocity, 0.25f);
     }
 }
