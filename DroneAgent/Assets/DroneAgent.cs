89ea6b7b649c6018bcbc26e278c35c4ba18d0dbd
nucontreras <48302692+nucontreras@users.noreply.github.com>
2023-03-04 21:46:00 +0100

    preparation to create heuristic ml-agents

--- DroneAgent/Assets/DroneAgent.cs
+++ DroneAgent/Assets/DroneAgent.cs
@@ -9,6 +9,7 @@ using Unity.MLAgents.Actuators;
 
 public class DroneAgent : Agent
 {
+    // Drone parameters
     Rigidbody ourDrone;
     public float upForce;
     private float movementForwardSpeed = 500f;  // 500
@@ -26,6 +27,9 @@ public class DroneAgent : Agent
     private float tiltAmountSideways;
     private float tiltAmountVelocity;
 
+    // Sensor part - in testing
+    private readonly bool useVectorObs = true;
+
     public override void Initialize()
     {
         //currentTime = startingTime;
@@ -39,7 +43,7 @@ public class DroneAgent : Agent
     {
         this.ourDrone.angularVelocity = Vector3.zero;
         this.ourDrone.velocity = Vector3.zero;
-        //this.transform.localPosition = new Vector3(0, 0.2f, 0);
+        this.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
         //this.ourDrone.transform.localRotation = Quaternion.Euler(0, 90, 0);
 
         //Checkpoint1.SetActive(true);
@@ -51,6 +55,126 @@ public class DroneAgent : Agent
         //currentTime = startingTime;
 
     }
+
+    //public override void CollectObservations(VectorSensor sensor)
+    //{
+    //    if (useVectorObs)
+    //    {
+    //        sensor.AddObservation(StepCount / (float)MaxStep);
+    //    }
+    //}
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+
+        //MoveAgent(actionBuffers.DiscreteActions);
+
+        MovementUpDown(actionBuffers.DiscreteActions);
+        MovementForward(actionBuffers.DiscreteActions);
+        Rotation(actionBuffers.DiscreteActions);
+        ClampingSpeedValues(actionBuffers.DiscreteActions);
+        Swerwe(actionBuffers.DiscreteActions);
+
+        ourDrone.AddRelativeForce(Vector3.up * upForce);
+        ourDrone.rotation = Quaternion.Euler(new Vector3(tiltAmountForward, currentYRotation, tiltAmountSideways));
+
+
+        // Fell off platform
+        //if (this.transform.localPosition.y < -0.5f)
+        //{
+        //    //Debug.Log("<0");
+        //    //Debug.Log(Vector3.Distance(this.transform.localPosition, Target.localPosition));
+        //    AddReward(-0.1f);
+        //    EndEpisode();
+        //}
+
+        //currentTime -= 1 * Time.deltaTime;
+        //txtCountdown.text = currentTime.ToString("F1");
+        //if (currentTime <= 0)
+        //{
+        //    currentTime = 0;
+        //    AddReward(-0.05f);
+        //    EndEpisode();
+        //}
+
+        // Penalty given each step to encourage agent to finish task quickly.
+        AddReward(-0.1f / MaxStep);
+    }
+
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var action = act[0];
+
+        switch (action)
+        {
+            case 1:
+                dirToGo = transform.forward * 1f;
+                break;
+            case 2:
+                dirToGo = transform.forward * -1f;
+                break;
+            case 3:
+                rotateDir = transform.up * 1f;
+                break;
+            case 4:
+                rotateDir = transform.up * -1f;
+                break;
+            case 5:
+                dirToGo = transform.right * -0.75f;
+                break;
+            case 6:
+                dirToGo = transform.right * 0.75f;
+                break;
+            case 7:
+                dirToGo = transform.right * -0.75f;
+                break;
+            case 8:
+                dirToGo = transform.right * 0.75f;
+                break;
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[0] = 3;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[0] = 4;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
+        else if (Input.GetKey(KeyCode.L))
+        {
+            discreteActionsOut[0] = 7;
+        }
+        else if (Input.GetKey(KeyCode.I))
+        {
+            discreteActionsOut[0] = 5;
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            discreteActionsOut[0] = 8;
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            discreteActionsOut[0] = 6;
+        }
+    }
+
+    // No ML Agents program
 
     //void Awake()
     //{
@@ -72,100 +196,101 @@ public class DroneAgent : Agent
     //    ourDrone.AddRelativeForce(Vector3.up * upForce);
     //    ourDrone.rotation = Quaternion.Euler(new Vector3(tiltAmountForward, currentYRotation, tiltAmountSideways));
     //}
-    //void MovementUpDown()
-    //{
-    //    if ((Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f || Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f))
-    //    {
-    //        if(Input.GetKey(KeyCode.I) || Input.GetKey(KeyCode.K))
-    //        {
-    //            ourDrone.velocity = ourDrone.velocity;
-    //        }
-    //        if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K) && !Input.GetKey(KeyCode.J) && !Input.GetKey(KeyCode.L))
-    //        {
-    //            ourDrone.velocity = new Vector3(ourDrone.velocity.x, Mathf.Lerp(ourDrone.velocity.y, 0, Time.deltaTime * 5), ourDrone.velocity.z);
-    //            upForce = 281;
-    //        }
-    //        if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K) && Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.L))
-    //        {
-    //            ourDrone.velocity = new Vector3(ourDrone.velocity.x, Mathf.Lerp(ourDrone.velocity.y, 0, Time.deltaTime * 5), ourDrone.velocity.z);
-    //            upForce = 110;
-    //        }
-    //        if (Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.L))
-    //        {
-    //            upForce = 410;
-    //        }
-    //    }
-    //    if ((Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f))
-    //    {
-    //        upForce = 135;
-    //    }
 
-    //    if (Input.GetKey(KeyCode.I))
-    //    {
-    //        upForce = 450f;
-    //        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
-    //        {
-    //            upForce = 500f;
-    //        }
-    //    }
-    //    else if (Input.GetKey(KeyCode.K))
-    //    {
-    //        upForce = -200f;
-    //    } 
-    //    else if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K) && (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f))
-    //    {
-    //        upForce = 98.1f;
-    //    }
-    //}
-    //void MovementForward()
-    //{
-    //    if(Input.GetAxis("Vertical") != 0)
-    //    {
-    //        ourDrone.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * movementForwardSpeed);
-    //        tiltAmountForward = Mathf.SmoothDamp(tiltAmountForward, 20 * Input.GetAxis("Vertical"), ref tiltVelocityForward, 0.1f);
-    //    }
-    //}
-    //void Rotation()
-    //{
-    //    if (Input.GetKey(KeyCode.J))
-    //    {
-    //        wantedYRotation -= rotateAmoutByKeys;
-    //    }
-    //    if (Input.GetKey(KeyCode.L))
-    //    {
-    //        wantedYRotation += rotateAmoutByKeys;
-    //    }
-    //    currentYRotation = Mathf.SmoothDamp(currentYRotation, wantedYRotation, ref rotationYVelocity, 0.25f);
-    //}
-    //void ClampingSpeedValues()
-    //{
-    //    if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
-    //    {
-    //        ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 10f, Time.deltaTime * 5f));
-    //    }
-    //    if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f)
-    //    {
-    //        ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 10f, Time.deltaTime * 5f));
-    //    }
-    //    if (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
-    //    {
-    //        ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 5f, Time.deltaTime * 5f));
-    //    }
-    //    if (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f)
-    //    {
-    //        ourDrone.velocity = Vector3.SmoothDamp(ourDrone.velocity, Vector3.zero, ref velocityToSmoothDampToZero, 0.95f);
-    //    }
-    //}
-    //void Swerwe()
-    //{
-    //    if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
-    //    {
-    //        ourDrone.AddRelativeForce(Vector3.right * Input.GetAxis("Horizontal") * sideMovementAmount);
-    //        tiltAmountSideways = Mathf.SmoothDamp(tiltAmountSideways, -20 * Input.GetAxis("Horizontal"), ref tiltAmountVelocity, 0.1f);
-    //    }
-    //    else
-    //    {
-    //        tiltAmountSideways = Mathf.SmoothDamp(tiltAmountSideways, 0, ref tiltAmountVelocity, 0.1f);
-    //    }
-    //}
+    public void MovementUpDown(ActionSegment<int> act)
+    {
+        if ((Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f || Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f))
+        {
+            if (Input.GetKey(KeyCode.I) || Input.GetKey(KeyCode.K))
+            {
+                ourDrone.velocity = ourDrone.velocity;
+            }
+            if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K) && !Input.GetKey(KeyCode.J) && !Input.GetKey(KeyCode.L))
+            {
+                ourDrone.velocity = new Vector3(ourDrone.velocity.x, Mathf.Lerp(ourDrone.velocity.y, 0, Time.deltaTime * 5), ourDrone.velocity.z);
+                upForce = 281;
+            }
+            if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K) && Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.L))
+            {
+                ourDrone.velocity = new Vector3(ourDrone.velocity.x, Mathf.Lerp(ourDrone.velocity.y, 0, Time.deltaTime * 5), ourDrone.velocity.z);
+                upForce = 110;
+            }
+            if (Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.L))
+            {
+                upForce = 410;
+            }
+        }
+        if ((Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f))
+        {
+            upForce = 135;
+        }
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            upForce = 450f;
+            if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+            {
+                upForce = 500f;
+            }
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            upForce = -200f;
+        }
+        else if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K) && (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f))
+        {
+            upForce = 98.1f;
+        }
+    }
+    public void MovementForward(ActionSegment<int> act)
+    {
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            ourDrone.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * movementForwardSpeed);
+            tiltAmountForward = Mathf.SmoothDamp(tiltAmountForward, 20 * Input.GetAxis("Vertical"), ref tiltVelocityForward, 0.1f);
+        }
+    }
+    public void Rotation(ActionSegment<int> act)
+    {
+        if (Input.GetKey(KeyCode.J))
+        {
+            wantedYRotation -= rotateAmoutByKeys;
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            wantedYRotation += rotateAmoutByKeys;
+        }
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, wantedYRotation, ref rotationYVelocity, 0.25f);
+    }
+    public void ClampingSpeedValues(ActionSegment<int> act)
+    {
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 10f, Time.deltaTime * 5f));
+        }
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f)
+        {
+            ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 10f, Time.deltaTime * 5f));
+        }
+        if (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 5f, Time.deltaTime * 5f));
+        }
+        if (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f)
+        {
+            ourDrone.velocity = Vector3.SmoothDamp(ourDrone.velocity, Vector3.zero, ref velocityToSmoothDampToZero, 0.95f);
+        }
+    }
+    public void Swerwe(ActionSegment<int> act)
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            ourDrone.AddRelativeForce(Vector3.right * Input.GetAxis("Horizontal") * sideMovementAmount);
+            tiltAmountSideways = Mathf.SmoothDamp(tiltAmountSideways, -20 * Input.GetAxis("Horizontal"), ref tiltAmountVelocity, 0.1f);
+        }
+        else
+        {
+            tiltAmountSideways = Mathf.SmoothDamp(tiltAmountSideways, 0, ref tiltAmountVelocity, 0.1f);
+        }
+    }
 }
