189d5a1fbc711b6bbd8f9bf85121609a2f2d786b
nucontreras <48302692+nucontreras@users.noreply.github.com>
2023-06-04 16:51:06 +0200

    runnning transitions code

--- HumanoidAgents/Assets/animationStateController.cs
+++ HumanoidAgents/Assets/animationStateController.cs
@@ -6,24 +6,37 @@ public class animationStateController : MonoBehaviour
 {
     Animator animator;
     int isWalkingHash;
+    int isRunningHash;
     void Start()
     {
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isRunning = animator.GetBool(isRunningHash);
         bool isWalking = animator.GetBool(isWalkingHash);
-        bool forwardPresses = Input.GetKey("w");
-        if (!isWalking && forwardPresses)
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if (!isWalking && forwardPressed)
         {
             animator.SetBool(isWalkingHash, true);
         }
-        if (isWalking && !forwardPresses)
+        if (isWalking && !forwardPressed)
         {
             animator.SetBool(isWalkingHash, false);
+        }
+        if (!isRunning && (forwardPressed && runPressed)) 
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
         }
     }
 }
