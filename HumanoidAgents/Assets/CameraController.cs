f5a20c3086f5d81221eb8124257798e921a133d5
nucontreras <48302692+nucontreras@users.noreply.github.com>
2023-06-20 21:14:17 -0400

    Start & Update functions

--- HumanoidAgents/Assets/CameraController.cs
+++ HumanoidAgents/Assets/CameraController.cs
@@ -12,5 +12,14 @@ public class CameraController : MonoBehaviour
     private float verticalRotation;
     private float horizontalRotation;
 
-
+    private void Start()
+    {
+        this.verticalRotation = this.transform.localEulerAngles.x;
+        this.horizontalRotation = this.transform.eulerAngles.y;
+    }
+    private void Update()
+    {
+        Look();
+        Debug.DrawRay(this.transform.position, this.transform.forward * 2, Color.red);
+    }
 }
